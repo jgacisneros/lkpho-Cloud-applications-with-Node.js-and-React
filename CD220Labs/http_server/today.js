5f8444326a111ff28e30c1da2139a83611713330
jgacisneros <jgacisneros@hotmail.com>
2024-10-19 15:21:08 -0700

    Fixing issue with getDate

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,6 @@
  // Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
-    // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+    // Get the current date and time in the timezone "America/Vancouver"
+    let vicTime = new Date();
+    return vicTime; // Return the formatted date and time
 };
